1d0e2da095fa45c39e3aa78cd153482885ec0b5e
hemlata gehlot <er.hemlata@gmail.com>
2021-05-26 21:56:12 +0530

    Looping Program Added

---
+++ Looping.py
@@ -0,0 +1,20 @@
+A=10
+B="HelloPython"
+C=6.9999
+D=1+2j
+
+#datatypes print datatypes
+print("A-"+str(A))
+print("B-"+str(B))
+print("C-"+str(C))
+print("C-"+str(D))
+
+#get data types 
+print("A type is ",end="")
+print(type(A))
+print("B type is ",end="")
+print(type(B))
+print("C type is ",end="")
+print(type(C))
+print("D type is ",end="")
+print(type(D))
